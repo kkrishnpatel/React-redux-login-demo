729f3485bd9731494d71b46b2bb2ddc25802ed80
Krishn Patel <er.krishnpatel@gmail.com>
2023-04-28 11:54:27 +0100

    db log added

--- Backend/db/mongoose.js
+++ Backend/db/mongoose.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-});
+}).then(() => {
+    console.log("Successfully connected to database");
+})
+    .catch((error) => {
+        console.error("database connection failed");
+        console.error(error);
+        process.exit(1);
+    });
 
 module.exports = { mongoose };
+
